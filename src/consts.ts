a39a3080949534ecb0e9fdbfd9b481f57553eaa6
Anonscholar <adyanshmishra@proton.me>
2024-11-04 19:44:13 +0530

    basic customisations

--- src/consts.ts
+++ src/consts.ts
@@ -13,13 +13,13 @@ export type Link = {
 }
 
 export const SITE: Site = {
-  TITLE: 'astro-erudite',
+  TITLE: 'Poetic Ramblings',
   DESCRIPTION:
-    'astro-erudite is a opinionated, no-frills blogging template—built with Astro, Tailwind, and shadcn/ui.',
-  EMAIL: 'jason@enscribe.dev',
+    'thoughts of a guy who wants to live.',
+  EMAIL: 'adyanshmishra@proton.me',
   NUM_POSTS_ON_HOMEPAGE: 2,
   POSTS_PER_PAGE: 3,
-  SITEURL: 'https://astro-erudite.vercel.app',
+  SITEURL: 'https://anonscholar.github.io',
 }
 
 export const NAV_LINKS: Link[] = [
@@ -30,8 +30,6 @@ export const NAV_LINKS: Link[] = [
 ]
 
 export const SOCIAL_LINKS: Link[] = [
-  { href: 'https://github.com/jktrn', label: 'GitHub' },
-  { href: 'https://twitter.com/enscry', label: 'Twitter' },
-  { href: 'jason@enscribe.dev', label: 'Email' },
-  { href: '/rss.xml', label: 'RSS' },
+  { href: 'https://github.com/Anonscholar', label: 'GitHub' },
+  { href: 'adyanshmishra@proton.me', label: 'Email' },
 ]
